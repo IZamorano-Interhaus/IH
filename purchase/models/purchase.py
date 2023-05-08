edbfd262d09f75a580109795995aa32837977b44
Ignacio <izamorano@interhaus.cl>
2023-05-08 16:30:59 -0400

    _get_contabilizado, testing 5, default value deleted

--- purchase/models/purchase.py
+++ purchase/models/purchase.py
@@ -133,7 +133,7 @@ class PurchaseOrder(models.Model):
         ('sin contabilizar','No contabilizado'),
         ('sin contabilizar 2','No contabilizado 2'),
         ('contabilizado','Contabilizado'),
-    ], string="Estado Contabilización OC", compute="_get_Contado", store=True, readonly=True, copy=False, default='sin contabilizar')
+    ], string="Estado Contabilización OC", compute="_get_Contado", store=True, readonly=True, copy=False)
     date_planned = fields.Datetime(
         string='Expected Arrival', index=True, copy=False, compute='_compute_date_planned', store=True, readonly=False,
         help="Delivery date promised by vendor. This date is used to determine expected arrival of products.")
