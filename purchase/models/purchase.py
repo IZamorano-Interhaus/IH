9711e613c793c8897cbd159cd87a6e9d337faf2a
Ignacio <izamorano@interhaus.cl>
2023-05-10 15:54:51 -0400

    patch code 1.2.2

--- purchase/models/purchase.py
+++ purchase/models/purchase.py
@@ -502,7 +502,7 @@ class PurchaseOrder(models.Model):
                 order.message_subscribe([order.partner_id.id])
             for move in self:
                 if any(
-                    move.env.ref['account.account_move.ref']!=order.name
+                    move.env['account.account_move.ref']!=order.name
                 ):
                     self.write({
                         
