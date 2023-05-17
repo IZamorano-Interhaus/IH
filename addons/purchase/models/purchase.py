1366fb2c72d52b6dfda849e85962775034ec60f3
Ignacio <izamorano@interhaus.cl>
2023-05-17 11:52:49 -0400

    cambio en el diccionario de valores que traera prepare draft

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -623,25 +623,28 @@ class PurchaseOrder(models.Model):
         """
         self.ensure_one()
         move_type = self._context.get('default_move_type', 'entry')
-
-        partner_invoice = self.env['res.partner'].browse(self.partner_id.address_get(['invoice'])['draft'])
+        
+        partner_draft = self.env['res.partner'].browse(self.partner_id.address_get(['invoice'])['draft'])
         partner_bank_id = self.partner_id.commercial_partner_id.bank_ids.filtered_domain(['|', ('company_id', '=', False), ('company_id', '=', self.company_id.id)])[:1]
-
+        autopost = self.env['account.move'].browse(self.partner_id._autopost_draft_entries())
+        
         draft_vals = {
-            'ref': self.partner_ref or '',
+            
             'date':self.date_order,
             'journal_id':self.product_id,
             'move_type': move_type,
-            'narration': self.notes,
+            'state':self.state,
+            'auto_post':autopost,
+            
             'currency_id': self.currency_id.id,
-            'invoice_user_id': self.user_id and self.user_id.id or self.env.user.id,
-            'partner_id': partner_invoice.id,
-            'fiscal_position_id': (self.fiscal_position_id or self.fiscal_position_id._get_fiscal_position(partner_invoice)).id,
+            'draft_user_id': self.user_id and self.user_id.id or self.env.user.id,
+            'partner_id': partner_draft.id,
+            'fiscal_position_id': (self.fiscal_position_id or self.fiscal_position_id._get_fiscal_position(partner_draft)).id,
             'payment_reference': self.partner_ref or '',
             'partner_bank_id': partner_bank_id.id,
-            'invoice_origin': self.name,
-            'invoice_payment_term_id': self.payment_term_id.id,
-            'invoice_line_ids': [],
+            'draft_origin': self.name,
+            'draft_payment_term_id': self.payment_term_id.id,
+            'draft_line_ids': [],
             'company_id': self.company_id.id,
         }
         return draft_vals
