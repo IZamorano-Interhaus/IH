383782520747b18fb04febf95f65aa6c884b6db4
Ignacio <izamorano@interhaus.cl>
2023-06-01 16:52:10 -0400

    se elimina modulodiscount 2

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -1137,51 +1137,8 @@ class PurchaseOrderLine(models.Model):
             "CHECK(display_type IS NULL OR (product_id IS NULL AND price_unit = 0 AND product_uom_qty = 0 AND product_uom IS NULL AND date_planned is NULL))",
             "Forbidden values on non-accountable purchase order line"),
     ]
+
     
-    fixed_discount = fields.Float(string="Fixed Disc.", digits="Product Price", default=0.000)
-
-    discount = fields.Float(string='% Disc.', digits='Discount', default=0.000)
-
-    @api.onchange("discount")
-    def _onchange_discount(self):
-        for line in self:
-            if line.discount != 0:
-                self.fixed_discount = 0.0
-                fixed_discount = (line.price_unit * line.product_qty) * (line.discount / 100.0)
-                line.update({"fixed_discount": fixed_discount})
-            if line.discount == 0:
-                fixed_discount = 0.000
-                line.update({"fixed_discount": fixed_discount})
-
-    @api.onchange("fixed_discount")
-    def _onchange_fixed_discount(self):
-        for line in self:
-            if line.fixed_discount != 0:
-                self.discount = 0.0
-                discount = ((self.product_qty * self.price_unit) - ((self.product_qty * self.price_unit) - self.fixed_discount)) / (self.product_qty * self.price_unit) * 100 or 0.0
-                line.update({"discount": discount})
-            if line.fixed_discount == 0:
-                discount = 0.0
-                line.update({"discount": discount})
-
-    def _prepare_compute_all_values(self):
-        # Hook method to returns the different argument values for the
-        # compute_all method, due to the fact that discounts mechanism
-        # is not implemented yet on the purchase orders.
-        # This method should disappear as soon as this feature is
-        # also introduced like in the sales module.
-        self.ensure_one()
-        price_unit_w_discount = self.price_unit
-        if self.discount != 0:
-            price_unit_w_discount = self.price_unit * (1 - (self.discount / 100.0))
-
-        return {
-            'price_unit': price_unit_w_discount,
-            'currency': self.order_id.currency_id,
-            'quantity': self.product_qty,
-            'product': self.product_id,
-            'partner': self.order_id.partner_id,
-        }
     @api.depends('product_qty', 'price_unit', 'taxes_id')
     def _compute_amount(self):
         for line in self:
